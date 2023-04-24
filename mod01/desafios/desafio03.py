a96da2b76d111c67f2a3a4ce53aae14d1c5a3197
git/Isabela Cartaxo <belacartaxo@gmail.com>
2023-04-24 16:38:10 -0300

    aula 4 python

---
+++ mod01/desafios/desafio03.py
@@ -0,0 +1,4 @@
+print('=======DESAFIO 03=======')
+n1 = input('Primrio número:')
+n2 = input('Segundo número:')
+print('A soma entre', n1, 'e', n2, 'é', int(n1)+int(n2))
---
+++ mod01/teste01.py
@@ -0,0 +1,4 @@
+nome = input('Qual é o seu nome?')
+idade = input('Quantos anos você tem?')
+nascimento = input('Em que ano você nasceu?')
+print('O meu nome é', nome, 'tenho', idade, 'anos e eu nasci em', nascimento)
