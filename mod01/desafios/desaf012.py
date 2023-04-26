e5ee4db99ee0d1a515c3bd48b93231fcada6f14e
git/Isabela Cartaxo <belacartaxo@gmail.com>
2023-04-26 15:36:30 -0300

    desafio 13

--- mod01/desafios/desaf012.py
+++ mod01/desafios/desaf012.py
@@ -1,4 +1,4 @@
 print('=======DESAFIO 12=======')
 n = float(input('Qual é o valor do produto? R$'))
-print('O valor do produto é R${}, e'.format(n), end='')
-print(' com 5% de desconto ele sai por R${}'.format(n*95/100))
+print('O valor do produto é R${:.2f}, e'.format(n), end='')
+print(' com 5% de desconto ele sai por R${:.2f}'.format(n*95/100))
--- mod01/desafios/desaf013.py
+++ mod01/desafios/desaf013.py
@@ -1,2 +1,4 @@
 print('=======DESAFIO 13=======')
-
+sal = float(input('Qual é o seu salário? R$'))
+print('O seu salário é de R${:.2f} '.format(sal), end='')
+print('e com 15% de aumento ele fica: R${:.2f}'.format(sal*115/100))
