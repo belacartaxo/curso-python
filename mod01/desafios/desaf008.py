d7fe29bd0f1a9750c5c7245cc17a65c54a725a83
git/Isabela Cartaxo <belacartaxo@gmail.com>
2023-04-26 16:09:05 -0300

    upgrade desafio 8

--- mod01/desafios/desaf008.py
+++ mod01/desafios/desaf008.py
@@ -1,3 +1,5 @@
 print('=======DESAFIO 08=======')
 n = float(input('Digite um valor (m) para saber a sua conversão em cm e mm: '))
-print('>> {}m correspondem a {:.2f}cm e {:.2f}mm'.format(n, (n*100), (n*1000)))
+print('>> {}km\n>> {}hm\n>> {}dam'.format(n/1000, n/100, n/10))
+print('>> {}m'.format(n))
+print('>> {}dm\n>> {}cm\n>> {}mm'.format(n*10, n*100, n*1000))
